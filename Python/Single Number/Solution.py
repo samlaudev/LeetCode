be59036cc26be0efe5771464229f83ce7faf4586
Sam Lau <samlaudev@gmail.com>
2016-11-07 10:01:14 +0800

    添加Python实现的Single Number

---
+++ Python/Single Number/Solution.py
@@ -0,0 +1,21 @@
+# Problem: Single Number
+#
+# Given an array of integers, every element appears twice except for one. Find that single one.
+#
+# Note:
+#
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+#
+################################################################################
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            result = result ^ num
+
+        return result
