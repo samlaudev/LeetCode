e3487cd3086aa9079bee99d0ed8f3dd4dddb6fbe
Sam Lau <samlaudev@gmail.com>
2016-11-08 10:20:34 +0800

    优化Python实现的First Bad Version

--- Python/First Bad Version/Solution.py
+++ Python/First Bad Version/Solution.py
@@ -16,6 +16,11 @@
 # @return a bool
 # def isBadVersion(version):
 
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
 class Solution(object):
     def firstBadVersion(self, n):
         """
@@ -27,11 +32,13 @@ class Solution(object):
 
         while low < high:
             mid = (low + high) / 2
-            if isBadVersion(mid) and not isBadVersion(mid-1):
-                return mid
-            elif isBadVersion(mid) and isBadVersion(mid-1):
-                high = mid - 1
-            elif not isBadVersion(mid):
+
+            if isBadVersion(mid):
+                if not isBadVersion(mid-1):
+                    return mid
+                else:
+                    high = mid -1
+            else:
                 low = mid + 1
 
         return low
