84feee05ff1da1f8b3ac7d44b0e96cd5e341496a
Sam Lau <samlaudev@gmail.com>
2016-11-17 10:47:48 +0800

    添加Python实现的Insert Delete GetRandom O(1)

---
+++ Python/Insert Delete GetRandom O(1)/Solution.py
@@ -0,0 +1,89 @@
+# Problem: Insert Delete GetRandom O(1)
+#
+# Design a data structure that supports all following operations in average O(1) time.
+#   1. insert(val): Inserts an item val to the set if not already present.
+#   2. remove(val): Removes an item val from the set if present.
+#   3. getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+#
+# Example:
+#
+# // Init an empty set.
+# RandomizedSet randomSet = new RandomizedSet();
+#
+# // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+# randomSet.insert(1);
+#
+# // Returns false as 2 does not exist in the set.
+# randomSet.remove(2);
+#
+# // Inserts 2 to the set, returns true. Set now contains [1,2].
+# randomSet.insert(2);
+#
+# // getRandom should return either 1 or 2 randomly.
+# randomSet.getRandom();
+#
+# // Removes 1 from the set, returns true. Set now contains [2].
+# randomSet.remove(1);
+#
+# // 2 was already in the set, so return false.
+# randomSet.insert(2);
+#
+# // Since 1 is the only number in the set, getRandom always return 1.
+# randomSet.getRandom();
+#
+################################################################################
+
+from collections import defaultdict
+from random import randint
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.__list = []
+        self.__used = defaultdict(int)
+
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        not_has = val not in self.__used
+        if not_has:
+            self.__list.append(val)
+            self.__used[val] = len(self.__list) - 1
+
+        return not_has
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+
+        if val not in self.__used:
+            return False
+
+        last = self.__list.pop()
+
+        if val != last:
+            index = self.__used[val]
+            self.__used[last] = index
+            self.__list[index] = last
+            del self.__used[val]
+        else:
+            del self.__used[last]
+
+        return True
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.__list[randint(0, len(self.__list) - 1)]
