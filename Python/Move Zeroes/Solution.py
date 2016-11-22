413103ab91fd76d69b20aa020605025be53110a5
Sam Lau <samlaudev@gmail.com>
2016-11-22 10:02:17 +0800

    添加Python实现的Move Zeroes

---
+++ Python/Move Zeroes/Solution.py
@@ -0,0 +1,24 @@
+# Problem: Move Zeroes
+#
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+#
+# For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+#
+# Note:
+#   1. You must do this in-place without making a copy of the array.
+#   2. Minimize the total number of operations.
+#
+################################################################################
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in xrange(len(nums)):
+            if nums[i] == 0:
+                for j in xrange(i+1, len(nums)):
+                    if nums[j] != 0:
+                        nums[i], nums[j] = nums[j], nums[i]
+                        break
